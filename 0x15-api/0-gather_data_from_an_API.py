7ae52d09414e86660e13a6c933a792026e64eef0
Wonders Victor <wondersprince1@gmail.com>
2024-02-19 17:04:22 +0100

    Pycode changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,48 +1,52 @@
 #!/usr/bin/python3
-'''for a given employee ID, returns information about his/her TODO list progress.'''
+'''
+    for a given employee ID, returns information about
+    his/her TODO list progress.
+'''
 from sys import argv, exit
 import requests
-
-import logging
-#logging.basicConfig(level=logging.DEBUG)
-
-if len(argv) < 2:
-    exit(-1)
-
-user_id = argv[1]
+# import logging
 
 
+# logging.basicConfig(level=logging.DEBUG)
 def get_username(user_id):
+    '''Gets username of employee with user_id'''
     user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    #print("Making request now...")
+    # print("Making request now...")
     response = requests.get(user_url)
     if response.status_code == 200:
-        #print("Request successful. Getting data now")
+        # print("Request successful. Getting data now")
         data = response.json()
         return data.get('name')
 
-username = get_username(user_id)
-print(username)
 
 def get_todos(user_id):
+    '''Retrieves todos of employee with user_id'''
     todo_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
     response = requests.get(todo_url)
     if response.status_code == 200:
         data = response.json()
         return data
 
-todos = get_todos(user_id)
-print(todos)
 
 def parse(username, todos):
+    '''Parses data'''
     completed = []
     total_tasks = 0
     for todo in todos:
         total_tasks += 1
-        if todo.get('completed') == True:
+        if todo.get('completed') is True:
             completed.append(todo.get('title'))
     completed_tasks = len(completed)
-    print(f"Employee {username} is done with tasks({completed_tasks/total_tasks})")
+    print(f"Employee {username} is done "
+          "with tasks({completed_tasks/total_tasks})")
     for task in completed:
         print(f"\t {task}")
 
+
+if len(argv) < 2:
+    exit(-1)
+
+user_id = argv[1]
+username = get_username(user_id)
+parse(username, user_id)
