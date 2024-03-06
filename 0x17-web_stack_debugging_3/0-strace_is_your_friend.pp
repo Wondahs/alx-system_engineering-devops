b52071e4941c1aec8e34d4f3e5d160defef89f59
Wonders Victor <wondersprince1@gmail.com>
2024-03-07 00:18:21 +0100

    Added sed paths

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Fix Apache Errors
 
 exec { 'replace_phpp_with_php':
-  command     => "/usr/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => 'usr/local/bin/:/bin/:/usr/bin/',
 }
